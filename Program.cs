9ced1f9a486fae7cd0fbb05f1099296cbff8d54c
William Bennett <bennettw@mwc-111126.dhcp.missouri.edu>
2020-02-07 14:06:43 -0600

    First commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IT_2040
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
