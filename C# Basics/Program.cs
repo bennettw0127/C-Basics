d40a5ed1085bf1500f516c0e110a9e527b05e580
William Bennett <bennettw@mwc-111126.dhcp.missouri.edu>
2020-02-07 21:03:54 -0600

    Final commit

--- C# Basics/Program.cs
+++ C# Basics/Program.cs
@@ -6,7 +6,65 @@ namespace C__Basics
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            const byte sample1 = 0x3A;
+            byte  sample2 = 58;
+            int heartRate = 85;
+            double deposits = 135002796;
+            const float acceleration = 9.800;
+            float mass = 14.6;
+            double distance = 129.763001;
+            bool lost = true;
+            bool expensive = true;
+            int choice = 2;
+            const char integral = \u222B;
+            const string greeting = "Hello";
+            string name = "Karen";
+            if (sample1 == sample2){
+                Console.WriteLine("The samples are equal.");
+            }
+            else{
+                Console.WriteLine("The samples are not equal.");
+            }
+            if (heartRate >= 40 && heartRate <= 80){
+                Console.WriteLine("Heart rate is not normal.");
+            }
+            else
+            {
+                Console.WriteLine("Heart rate is normal.");
+            }
+            if (deposits > 100000000){
+                Console.WriteLine("You are exceedingly wealthy");
+            }
+            else{
+                Console.WriteLine("Sorry you are so poor.");
+            }
+            float force = mass * acceleration;
+            Console.WriteLine(force);
+
+            Console.WriteLine(distance.ToString(), "is the distance.");
+
+            if (lost == true && expensive == true){
+                Console.WriteLine("I am really sorry! I will get the manager.");
+            }
+            else{
+                Console.WriteLine("Here is coupon for 10% off.");
+            }
+            switch (choice)
+            {
+                case 1:
+                    Console.WriteLine("You chose 1.");
+                    break;
+                case 2:
+                    Console.WriteLine("You chose 2");
+                    break;
+                case 3:
+                    Console.WriteLine("You chose 3.");
+                    break;
+                case 4:
+                    Console.WriteLine("You made an unknown choice.");
+                    break;
+            }
+            
         }
     }
 }
